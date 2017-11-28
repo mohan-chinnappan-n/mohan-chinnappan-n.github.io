6437fce82e836bc7439ed2aefbdb54934fdf0573
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2017-11-28 11:29:10 -0500

    source code added

---
+++ sfdc/demo/pe/comp/EventSubscriberConsoleController.js
@@ -0,0 +1,68 @@
+({
+    onCometdLoaded : function (component, event, helper){
+        var cometd = new org.cometd.CometD() // from cometd.js
+        // set this cometd object in the component attribute
+        component.set('v.cometd', cometd);
+        if (component.get('v.sessionId') != null) {
+            // let us connect to cometd
+            helper.connectCometd(component)
+        }
+
+    },
+
+    onInit : function(component, event, helper) {
+        /*
+         * component.set('v.notifications', [
+            {  message: 'Furnace Power ON'},
+            {  message: 'Furnace Power Off'},
+
+
+        ]);
+        */
+
+         component.set('v.cometdSubscriptions',[]);
+         component.set('v.notifications',[]);
+
+         // disconnect cometd connection when leaving page
+         window.addEventListener('unload', function(event){
+             helper.disconnectCometd(component)
+         })
+
+         // get the server-side apex @AuraEnabled method
+         var action = component.get('c.getSessionId');
+         // callback
+         action.setCallback(this, function(res){
+             // successful response?
+             if (component.isValid() && res.getState() === 'SUCCESS') {
+                 // store the sessionId in the component attribute
+                 component.set('v.sessionId', res.getReturnValue());
+                 // cometd client is available?
+                 if (component.get('v.cometd') != null){
+                     helper.connectCometd(component);
+                 }
+             } else {
+                 console.error(res);
+             }
+         })
+         // queue it for async execution
+         $A.enqueueAction(action);
+
+
+         helper.displayToast(component, 'succes', 'Ready to receive Furnace Power Notifictions');
+
+
+     },
+
+    onClear: function(component, event, helper) {
+        component.set('v.notifications',[]);
+    },
+
+    onToggleMute: function(component, event, helper){
+        var isMuted = component.get('v.isMuted');
+        component.set('v.isMuted', !isMuted);
+        helper.dispalyToast(compoent, 'success', "Notification: " + (!isMuted ? 'muted' : 'unmuted'))
+    }
+
+
+
+})
