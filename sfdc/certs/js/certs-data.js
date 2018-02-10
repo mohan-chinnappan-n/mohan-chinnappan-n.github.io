ae1e1adfd756ea32b417bfb83da19e2b7b0d1cfe
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2018-02-10 16:45:51 -0500

    dx content added

---
+++ sfdc/certs/js/certs-data.js
@@ -0,0 +1,86 @@
+var treeData = [
+  {
+    "name": "SFDC Certs",
+    "parent": "null",
+    "children": [
+      { "name": "Administrators",
+        "children": [
+          { "name": "Administrator", "url": "http://certification.salesforce.com/administrator" },
+          { "name": "Advanced Administrator" }
+        ]
+      },
+
+
+
+    { "name": "Architects",
+        "children": [
+          { "name": "Data Architecture  & Management" },
+          { "name": "Development Lifecycle & Deployment Management" },
+
+          { "name": "Identity & Access Management Designer" },
+          { "name": "Integration Architecture Designer" },
+          { "name": "Mobile Solution Architecture Designer" },
+          { "name": "Sharing & Visibility Designer" },
+
+          { "name": "Application Architect" },
+          { "name": "System Architect" },
+          { "name": "Technical Architect" }
+        ]
+    },
+
+    { "name": "Consultants",
+        "children": [
+          { "name": "Sales Cloud Consultant" },
+          { "name": "Service Cloud Consultant" },
+          { "name": "Marketing Cloud Consultant" },
+
+          { "name": "Communtiy Cloud Consultant" },
+          { "name": "Field Service Lightning Cloud Consultant" },
+          { "name": "Pardot Consultant" },
+
+        ]
+    },
+
+    { "name": "Developers",
+        "children": [
+          { "name": "Commerce Cloud Digital Developer" },
+          { "name": "Platform Developer I" },
+          { "name": "Platform Developer II" },
+
+        ]
+    },
+
+
+
+    { "name": "Marketers",
+        "children": [
+          { "name": "Marketing Cloud Consultant" },
+          { "name": "Marketing Cloud Social Specialist" },
+          { "name": "Marketing Cloud Email Specialist" }
+
+        ]
+    },
+
+    { "name": "Pardot Experts",
+        "children": [
+          { "name": "Pardot Consultant" },
+          { "name": "Pardot Specialist" }
+
+        ]
+    },
+    { "name": "Specialists",
+        "children": [
+          { "name": "CPQ Specialistr" }
+        ]
+    },
+
+
+
+
+
+
+
+    ]
+
+      }
+]
