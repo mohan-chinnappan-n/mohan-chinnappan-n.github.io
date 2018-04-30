c5653bd1b79a6b3eccb906be4dea407c6b1c08e6
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2018-04-30 08:17:21 -0400

    added ui-select

--- sfdc/data/bigobjects/bobj-editor.js
+++ sfdc/data/bigobjects/bobj-editor.js
@@ -158,7 +158,8 @@ $scope.prepString  = function() {
 $scope.restGet = function(url) {
 
   var headers = {
-    "Authorization": "Bearer " + $scope.oauth2Parms.access_token
+    "Authorization": "Bearer " + $scope.oauth2Parms.access_token,
+    'Content-Type: application/json'
   };
   if (url === undefined) url = $scope.oauth2Parms.instance_url + '/services/data/v' + $scope.apiVersion + '.0/';
   console.log(url);
