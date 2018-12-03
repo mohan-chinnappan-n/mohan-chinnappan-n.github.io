3e3fcda5f6d46bed65b4d243b4043c0cef6d028a
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2018-12-02 21:27:17 -0500

    google paper added

--- sw2/js/book.js
+++ sw2/js/book.js
@@ -1,10 +1,27 @@
 // book.js
-Split(['#menu', '#content'],  {sizes: [20, 80]});
-var converter = new showdown.Converter();
-var eles = document.getElementsByClassName('md');
-for(var i = 0; i < eles.length; i++) {
-         var content = eles[i].innerHTML;
-         eles[i].innerHTML =  converter.makeHtml(content);
-}
 
+
+(function() { 
+   
+    // set the titles 
+    var title = document.getElementById('title');
+    title.innerHTML = config.title;
+    var appName = document.getElementById('appName');
+    appName.innerHTML = config.title;
+    var mainTopic = document.getElementById('mainTopic');
+    mainTopic.innerHTML = config.title;
+    var contentTitle = document.getElementById('contentTitle');
+   contentTitle.innerHTML = config.title;
+ 
+
+
+    Split(['#menu', '#content'],  {sizes: [20, 80]});
+    var converter = new showdown.Converter();
+    var eles = document.getElementsByClassName('md');
+    for(var i = 0; i < eles.length; i++) {
+             var content = eles[i].innerHTML;
+             eles[i].innerHTML =  converter.makeHtml(content);
+    }
+
+})();
 
