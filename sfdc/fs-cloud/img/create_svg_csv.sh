88b8ce5dcaa2ada8b8fd0eeb69f73481a882d62e
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2020-04-16 07:08:55 -0400

    pe object added

--- sfdc/fs-cloud/img/create_svg_csv.sh
+++ sfdc/fs-cloud/img/create_svg_csv.sh
@@ -1,1 +1,1 @@
-sfdx mohanc:md:describe   -u mohan.chinnappan.n-a4en@force.com  -s  $1 -e $1.svg > $1.csv 
+sfdx mohanc:md:describe   -u mohan.chinnappan.n-q2x9@force.com  -s  $1 -e $1.svg > $1.csv 
