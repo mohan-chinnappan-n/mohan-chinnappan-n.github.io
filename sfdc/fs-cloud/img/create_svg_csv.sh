092e6f4f22d43f1d13625e991c55e56bfa85f4a9
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2021-08-16 23:45:34 -0400

    sr added

--- sfdc/fs-cloud/img/create_svg_csv.sh
+++ sfdc/fs-cloud/img/create_svg_csv.sh
@@ -1,2 +1,3 @@
-export un=mohan.chinnappan.n_ea2@gmail.com
+#export un=mohan.chinnappan.n_ea2@gmail.com
+export un=mchinnappan@fsc-gs0.com
 sfdx mohanc:md:describe   -u $un  -s  $1 -e $1.svg > $1.csv 
